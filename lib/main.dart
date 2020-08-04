57baa501388fe2d6eb28c6d39cf1b5eab6f562f5
Marto_0 <martvanenck1@gmail.com>
2020-08-04 19:18:41 +0200

    I forgot

--- lib/main.dart
+++ lib/main.dart
@@ -15,40 +15,32 @@ import 'package:theme_mode_handler/theme_mode_handler.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 void main() {
-  
   Crashlytics.instance.enableInDevMode = true;
 
   // Pass all uncaught errors from the framework to Crashlytics.
   FlutterError.onError = Crashlytics.instance.recordFlutterError;
-  
-  
+
   runZoned(() {
     runApp(App());
   }, onError: Crashlytics.instance.recordError);
-
 }
 
 class App extends StatefulWidget {
-
-  _AppState myAppState= new _AppState();
+  _AppState myAppState = new _AppState();
   @override
   _AppState createState() => myAppState;
-
 }
 
 class _AppState extends State<App> {
-
   String uid;
   bool authSignedIn;
   bool darkTheme = false;
   bool lightTheme = false;
-  
 
   @override
   void initState() {
     super.initState();
     getUserInfo();
-    
   }
 
   Future getTheme() async {
@@ -82,40 +74,38 @@ class _AppState extends State<App> {
     //setState(() {});
   }
 
-  
   @override
   Widget build(BuildContext context) {
-
     return ThemeModeHandler(
-      manager: ThemeModeManager(),
-      builder: (ThemeMode themeMode) {
-        return MaterialApp(
-          title: 'Keeping track',
-          theme: ThemeData.light().copyWith(
-            primaryColor: Color.fromARGB(255, 56, 113, 229),
-            accentColor: Color.fromARGB(255, 56, 113, 229),
-            cardColor: Colors.white,
-            scaffoldBackgroundColor: Colors.grey[200],
-          ),
-          darkTheme: ThemeData.dark().copyWith(
-            scaffoldBackgroundColor: Colors.grey[900],
-            cardColor: Colors.grey[800],
-            primaryColor: Color.fromARGB(255, 56, 113, 229),
-            accentColor: Color.fromARGB(255, 56, 113, 229),
-          ),
-          themeMode: themeMode,
-          home: (uid != null && authSignedIn != false) ? LoadingPage() : LoginPage(),
-          routes: {
-            '/login': (context) => LoginPage(),
-            '/loading': (context) => LoadingPage(),
-            '/days': (context) => DaysPage(),
-            '/day': (context) => DayPage(),
-            '/settings': (context) => SettingsPage(),
-            '/dayValuesSettings': (context) => DayValuesSettingsPage(),
-            '/about': (context) => AboutPage(),
-          }
-        );
-      }
-    );
+        manager: ThemeModeManager(),
+        builder: (ThemeMode themeMode) {
+          return MaterialApp(
+              title: 'Keeping track',
+              theme: ThemeData.light().copyWith(
+                primaryColor: Color.fromARGB(255, 56, 113, 229),
+                accentColor: Color.fromARGB(255, 56, 113, 229),
+                cardColor: Colors.white,
+                scaffoldBackgroundColor: Colors.grey[200],
+              ),
+              darkTheme: ThemeData.dark().copyWith(
+                scaffoldBackgroundColor: Colors.grey[900],
+                cardColor: Colors.grey[800],
+                primaryColor: Color.fromARGB(255, 56, 113, 229),
+                accentColor: Color.fromARGB(255, 56, 113, 229),
+              ),
+              themeMode: themeMode,
+              home: (uid != null && authSignedIn != false)
+                  ? LoadingPage()
+                  : LoginPage(),
+              routes: {
+                '/login': (context) => LoginPage(),
+                '/loading': (context) => LoadingPage(),
+                '/days': (context) => DaysPage(),
+                '/day': (context) => DayPage(),
+                '/settings': (context) => SettingsPage(),
+                '/dayValuesSettings': (context) => DayValuesSettingsPage(),
+                '/about': (context) => AboutPage(),
+              });
+        });
   }
 }
--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -4,64 +4,59 @@ import 'package:keeping_track/main.dart';
 import 'package:get/get.dart';
 import 'package:theme_mode_handler/theme_mode_handler.dart';
 
-
 class SettingsPage extends StatefulWidget {
-	@override
-	_SettingsPageState createState() => _SettingsPageState();
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  Map data = {};
+  bool darkThemeSwitch = true;
 
-	Map data = {};
-	bool darkThemeSwitch = true;
+  @override
+  Widget build(BuildContext context) {
+    if (!data.isNotEmpty) data = ModalRoute.of(context).settings.arguments;
 
-	@override
-	Widget build(BuildContext context) {
-
-		if(!data.isNotEmpty) data = ModalRoute.of(context).settings.arguments;
-
-		return Scaffold(
-			appBar: AppBar(
-				title: new Text(
-					'Settings',
-					style: TextStyle(
-						fontSize: 30,
-					)
-				),
-				elevation: 0,
-				centerTitle: true,
-			),
-			body: ListView(
-				padding: EdgeInsets.all(20),
-				children: <Widget>[
-					ListTile(
-						title: Text('change Day values'),
-						leading: Icon(Icons.event_note),
-						onTap: () {
-							Navigator.pushNamed(context, '/dayValuesSettings', arguments: data);
-						}
-					),
-					ListTile(
-						title: Row(
-							children: <Widget>[
-								Text('Dark mode'),
-								Switch(
-									value: ThemeModeHandler.of(context).themeMode == ThemeMode.dark,
-									onChanged: (boolVal) {
-										if(boolVal) ThemeModeHandler.of(context).saveThemeMode(ThemeMode.dark);
-										else ThemeModeHandler.of(context).saveThemeMode(ThemeMode.light);
-										setState(() {
-											darkThemeSwitch = boolVal;
-                      Crashlytics.instance.log('baz');
-                      Crashlytics.instance.crash();
-										});
-									},
-								)
-							] 
-						),
-					),
-				],
-			),
-		);
-	}
+    return Scaffold(
+      appBar: AppBar(
+        title: new Text('Settings',
+            style: TextStyle(
+              fontSize: 30,
+            )),
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: <Widget>[
+          ListTile(
+              title: Text('change Day values'),
+              leading: Icon(Icons.event_note),
+              onTap: () {
+                Navigator.pushNamed(context, '/dayValuesSettings',
+                    arguments: data);
+              }),
+          ListTile(
+            title: Row(children: <Widget>[
+              Text('Dark mode'),
+              Switch(
+                value: ThemeModeHandler.of(context).themeMode == ThemeMode.dark,
+                onChanged: (boolVal) {
+                  if (boolVal)
+                    ThemeModeHandler.of(context).saveThemeMode(ThemeMode.dark);
+                  else
+                    ThemeModeHandler.of(context).saveThemeMode(ThemeMode.light);
+                  setState(() {
+                    darkThemeSwitch = boolVal;
+                    Crashlytics.instance.log('baz');
+                    Crashlytics.instance.crash();
+                  });
+                },
+              )
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
 }
